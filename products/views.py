f131e8b92e9ff0db1168f22798b021665e074999
LucaFabbozzo <lucafabbozzo@gmail.com>
2024-01-11 10:25:10 +0100

    django template

--- products/views.py
+++ products/views.py
@@ -2,18 +2,25 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
+
+# def index(request):
+#     products = Product.objects.all().values()
+
+#     #JsonResponse funziona soltanto con dati che corrispondano a dizionari
+#     #bisogna quindi cambiare il tipo di dato in una lista con list()
+
+#     return JsonResponse(list(products), safe=False)
+
+# #questo caso è utilizzato soltanto se si vuole utilizzare un prototipo rapido e se si vuole costruire qualcosa di serio meglio utilizzare una dipendenza di terzi
+
+
+
+#funzioni per poter utilizzare il template di Django
 def index(request):
-    products = Product.objects.all().values()
-    # #con .filter() possiamo attivare un piccolo filtro passandogli come argomento quello che vorrei far ritornare
-    # productos = Product.objects.filter(puntaje=3)
-    # #se voglio una cosa in particolare devo utilizzare get() in questo caso ad esempio primary key = 3
-    # productos = Product.objects.get(pk=3)
-    #print(products)
+    products = Product.objects.all()
 
-    #JsonResponse funziona soltanto con dati che corrispondano a dizionari
-    #bisogna quindi cambiare il tipo di dato in una lista con list()
-    return JsonResponse(list(products), safe=False)
-
-
-#questo caso è utilizzato soltanto se si vuole utilizzare un prototipo rapido e se si vuole costruire qualcosa di serio meglio utilizzare una dipendenza di terzi
+    return render(
+        request,
+        'index.html',
+        context={'products': products}
+    )
