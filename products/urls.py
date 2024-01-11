00cad3378cb5baeedb38e555a5d690acdc9f0f15
LucaFabbozzo <lucafabbozzo@gmail.com>
2024-01-11 14:10:43 +0100

    links

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+#se esiste questa variabile, django va a concatenare il nome di questa variabile con il nome della rotta
+app_name = 'products'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:product_id>', views.detail, name='product_detail')
+    path('<int:product_id>', views.detail, name='detail')
 ]
